d25cc999ae0eb5d5b2c487cbd331518b3be99925
Radiantly <44368997+radiantly@users.noreply.github.com>
2020-11-01 11:28:50 +0000

    Deploy site using CF

--- link-shortener/index.js
+++ link-shortener/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 
 const app = express();
 
 app.use(express.static('public'))
 
-app.post()
+// app.post()
 
-app.listen(3000)
+app.listen(80)
+
+https.createServer({
+    key: fs.readFileSync('certs/privatekey.pem'),
+    cert: fs.readFileSync('certs/certificate.pem')
+}, app).listen(443);
