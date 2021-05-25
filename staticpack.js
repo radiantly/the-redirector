87e0c295d82aeba3e30ab74ba5f286805e39632f
radiantly <buildingsomethingfun@gmail.com>
2021-05-25 22:23:02 +0530

    Set build configuration
    With this, wrangler now watches only the ./src directory for changes,
    removing the need for wacky workaround scripts in package.json
    
    This is also meant we could remove the --watch flag from staticpack.js
    
    Updated Prettier version

--- staticpack.js
+++ staticpack.js
@@ -1,21 +1,24 @@
 /*
  * This is Staticpack, the successor of Webpack Jr.
- * Usage: node staticpack.js [--watch] entry.js bundle.js
+ * Usage: node staticpack.js entry.js bundle.js
  * Created by @radiantly
  */
 
 const fs = require("fs");
 const path = require("path");
-const fsDirs = new Set();
 const pack = (entry, bundle) => {
+  // Read entry file
   const fileContents = fs.readFileSync(entry, "utf-8");
+
+  // Change cwd to match entry file
+  process.chdir(path.parse(entry).dir);
+
+  // Build bundle
   fs.writeFileSync(
     bundle,
-    fileContents.replace(/`import (.+?)`/g, (match, p1) => {
-      const filePath = path.resolve(p1);
-      fsDirs.add(path.dirname(filePath));
+    fileContents.replace(/`import (.+?)`/g, (_, importpath) => {
       const replacedContents = fs
-        .readFileSync(p1, "utf-8")
+        .readFileSync(importpath, "utf-8")
         .replace(/`.*?`/g, match => match.replace(/(?<!\\)[`$]/g, "\\$&"));
       return "`" + replacedContents + "`";
     })
@@ -23,26 +26,7 @@ const pack = (entry, bundle) => {
 };
 
 const args = process.argv.slice(2);
-let watch = false;
-const watchArgIndex = args.indexOf("--watch");
-if (watchArgIndex != -1) {
-  watch = true;
-  args.splice(watchArgIndex, 1);
-}
-
 if (args.length !== 2) throw new Error("Incorrect number of arguments");
 
-const [entryFilePath, bundleFilePath] = args.map(arg => path.resolve(arg));
-
-if (!fs.existsSync(entryFilePath)) throw new Error(`Cannot find ${entryFilePath}`);
-pack(...args);
-if (watch) {
-  fsDirs.add(path.dirname(entryFilePath));
-  fsDirs.forEach(fsDir => {
-    fs.watch(fsDir, (eventType, filename) => {
-      if (path.resolve(filename) == bundleFilePath) return;
-      console.log(filename, ": ", eventType);
-      pack(...args);
-    });
-  });
-}
+// Convert relative paths to absolute paths
+pack(...args.map(arg => path.resolve(arg)));
